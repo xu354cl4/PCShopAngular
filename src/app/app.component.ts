682bf81ad4e0228a8bc323fe5dba0abeec666314
veryaden <veryaden@gmail.com>
2025-12-09 09:29:16 +0800

    user相關基礎模板

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginpageComponent } from "./loginpage/loginpage.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginpageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
