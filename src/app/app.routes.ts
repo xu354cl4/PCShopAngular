6126e2fd8bde20481091e9c71b9197406de31e76
xu354cl4 <leehao891118@gmail.com>
2025-12-09 15:52:19 +0800

    再合一下

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+  },
+  {
     path: 'home',
     loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
   },
