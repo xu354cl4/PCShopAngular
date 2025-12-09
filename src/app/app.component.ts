5b19c9c0ef41555326988cd97e5b634bd7affc87
xu354cl4 <leehao891118@gmail.com>
2025-12-09 13:27:55 +0800

    移除白癡layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from './layout/footer/footer.component';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { GamehomeComponent } from './gamehome/gamehome.component';
 
@@ -11,13 +11,14 @@ import { GamehomeComponent } from './gamehome/gamehome.component';
   standalone: true,
   imports: [
     CommonModule,
+    RouterOutlet,
     RouterModule,
     HeaderComponent,
     FooterComponent,
-    MatDialogModule,
-    RouterOutlet
+    MatDialogModule
   ],
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   constructor(private dialog: MatDialog) { }
