a28c5c341bc9cca4be8cbb5e6bf3a72d70e364f8
ycc0738 <abc0500999148@gmail.com>
2025-12-30 16:37:18 +0800

    首頁雛形

--- src/app/product/models/product-filter.model.ts
+++ src/app/product/models/product-filter.model.ts
@@ -1,5 +1,5 @@
 export interface ProductFilter {
-  minPrice: number | null;      // 最低價
-  maxPrice: number | null;      // 最高價
-  categories: string[];         // 選取的商品分類
+  minPrice?: number | null;      // 最低價
+  maxPrice?: number | null;      // 最高價
+  categories?: string[];         // 選取的商品分類
 }
