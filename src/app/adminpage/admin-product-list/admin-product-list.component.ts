3ea234fd87d637351b01684d21e374940dbc9b4a
ycc0738 <abc0500999148@gmail.com>
2026-01-02 16:37:58 +0800

    新增後台架構 admin

---
+++ src/app/adminpage/admin-product-list/admin-product-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-product-list',
+  imports: [],
+  templateUrl: './admin-product-list.component.html',
+  styleUrl: './admin-product-list.component.css'
+})
+export class AdminProductListComponent {
+
+}
