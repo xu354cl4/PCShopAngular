d2c34266b6794be6b8f27b14f8c42d89137ad213
xu354cl4 <leehao891118@gmail.com>
2026-01-02 09:47:41 +0800

    1

--- src/app/Services/auth-state.service.ts
+++ src/app/Services/auth-state.service.ts
@@ -113,17 +113,6 @@ export class AuthStateService {
   }
 
   //前端模擬user / admin身分 這邊直接綁死信箱
-  getCurrentUserRole(): UserRole {
-    const user = this.getCurrentUser();
-
-    // Demo 用規則（你之後可以拿掉）
-    if (user?.mail === 'ss860505@gmail.com') {
-      return 'Admin';
-    }
-
-    return 'User';
-  }
-
   getrole(mail: string): UserRole {
     const user = this.getCurrentUser();
 
